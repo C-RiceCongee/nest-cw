68940870a437d1790cfbd5fb4c3158249966eab3
#24 <1933310737@qq.com>
2023-10-15 23:34:57 +0800

    add:@nest/config load env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-
+import {ConfigModule} from '@nestjs/config';
+import config from './config';
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [...config],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
+
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {}
   @Get()
   getUsers(): any {
+    console.log(this.configService.get('db'));
+    console.log(this.configService.get('app'));
     return this.userService.getUsers();
   }
 }
